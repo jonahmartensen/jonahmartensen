4e6e9ba4bdeb0491439efd329ad6cba1d63caaa0
jonahmartensen <martensenjonah@gmail.com>
2026-02-09 16:17:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const img = preview.querySelector('img');
 const text = preview.querySelector('.preview-text');
 const links = document.querySelectorAll('.works-sidebar a');
 
+const baseScale = 1;
+
 links.forEach(link => {
   link.addEventListener('mouseenter', () => {
     img.src = link.dataset.image;
     text.textContent = link.dataset.text;
 
-    const scale = link.dataset.scale || 1;
-    img.style.transform = `scale(${scale})`;
+    const scale = parseFloat(link.dataset.scale) || 1;
+    img.style.transform = `scale(${baseScale * scale})`;
 
     preview.classList.add('visible');
   });
@@ -19,3 +21,37 @@ links.forEach(link => {
   });
 });
 
+
+const isTouch = window.matchMedia('(hover: none)').matches;
+
+links.forEach(link => {
+  if (isTouch) {
+    link.addEventListener('click', e => {
+      if (!preview.classList.contains('visible')) {
+        e.preventDefault();
+
+        img.src = link.dataset.image;
+        text.textContent = link.dataset.text;
+
+        const scale = parseFloat(link.dataset.scale) || 1;
+        img.style.transform = `scale(${scale})`;
+
+        preview.classList.add('visible');
+      }
+    });
+  } else {
+    link.addEventListener('mouseenter', () => {
+      img.src = link.dataset.image;
+      text.textContent = link.dataset.text;
+
+      const scale = parseFloat(link.dataset.scale) || 1;
+      img.style.transform = `scale(${scale})`;
+
+      preview.classList.add('visible');
+    });
+
+    link.addEventListener('mouseleave', () => {
+      preview.classList.remove('visible');
+    });
+  }
+});
